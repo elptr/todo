cc207f0857b064e11d6824a1439e38fa048a67b5
elptr <lena.petrova.v@gmail.com>
2025-12-03 18:49:21 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className='bg-gray-200 w-full h-screen flex items-center justify-center'>
+            To-do App
+        </div>
+    )
+}
+
+export default App;
